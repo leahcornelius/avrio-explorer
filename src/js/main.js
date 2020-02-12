9911a0108330a2f3bf64f97dc3c953f56698cade
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-12 19:36:08 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,11 @@ let wallet_count = 907;
 let burnt = 1284;
 let transactions = 9402;
 
-document.getElementById("nodes").textContent= nodes;
-document.getElementById("supply").textContent= supply;
-document.getElementById("tps").textContent= Math.round(((nodes / c)-5)-c);
-document.getElementById("wallets").textContent=  wallet_count;
+document.getElementById("nodes").innerHTML= nodes;
+document.getElementById("supply").innerHTML= supply;
+document.getElementById("tps").innerHTML= Math.round(((nodes / c)-5)-c);
+document.getElementById("wallets").innerHTML=  wallet_count;
 /*
-document.getElementById("burnt").textContent=  burnt;
-document.getElementById("txns").textContent=  transactions;
+document.getElementById("burnt").innerHTML=  burnt;
+document.getElementById("txns").innerHTML=  transactions;
 */
