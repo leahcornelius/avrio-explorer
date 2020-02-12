e55204b85275b951e7576bd0b1f4bbca7482d344
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-12 20:03:14 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,19 @@ document.getElementById("wallets").innerHTML=  wallet_count;
 document.getElementById("burnt").innerHTML=  burnt;
 document.getElementById("txns").innerHTML=  transactions;
 */
+function addBlock(height,sender,txn_count,tfc, recipitents,timestamp) {
+   
+    let a = new Date(timestamp * 1000);
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
+    let time = `${date}/${month}/${year} ${hour}:${min}:${sec}`;
+   document.getElementById('blockList').innerHTML = " <tr> <td>"+height+"</td> <td>"+sender+"</td> <td>"+txn_count+"</td> <td>"+ tfc+"AIO</td> <td>"+recipitents+"</td> <td>"+time+"</td> </tr>" + document.getElementById('blockList').innerHTML;;
+}
+
+
+addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
+addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
