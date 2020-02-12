09a8b8298583823126b33cbad9dcd0d78f2697a5
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-12 20:18:11 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ document.getElementById("wallets").innerHTML=  wallet_count;
 document.getElementById("burnt").innerHTML=  burnt;
 document.getElementById("txns").innerHTML=  transactions;
 */
-document.getElementById('blockList').innerHTML = "<tr> <th>Height</th> <th>Sender</th> <th>Txn Count</th> <th>Total Funds Change</th> <th>Recipitents</th> <th>Timestanp</th> </tr> ";
+document.getElementById('blockList').innerHTML = " <tr> <th>Height</th> <th>Sender</th> <th>Txn Count</th> <th>Total Funds Change</th> <th>Recipitents</th> <th>Timestanp</th> </tr> ";
 function addBlock(height,sender,txn_count,tfc, recipitents,timestamp) {
    
     let a = new Date(timestamp * 1000);
@@ -24,7 +24,7 @@ function addBlock(height,sender,txn_count,tfc, recipitents,timestamp) {
     let min = a.getMinutes();
     let sec = a.getSeconds();
     let time = `${date}/${month}/${year} ${hour}:${min}:${sec}`;
-   document.getElementById('blockList').innerHTML = " <tr> <td>"+height+"</td> <td>"+sender+"</td> <td>"+txn_count+"</td> <td>"+ tfc+" AIO</td> <td>"+recipitents+"</td> <td>"+time+"</td> </tr> \n" + document.getElementById('blockList').innerHTML;
+   document.getElementById('blockList').innerHTML = ocument.getElementById('blockList').innerHTML + " <tr> <td>"+height+"</td> <td>"+sender+"</td> <td>"+txn_count+"</td> <td>"+ tfc+" AIO</td> <td>"+recipitents+"</td> <td>"+time+"</td> </tr> ";
 }
 
 
