59b7793f2d56860b8d773d549c2c2267d71eedd9
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-03-05 19:09:43 +0000

    update tps calc code

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,16 @@
-let nodes = 837;
-let supply = 7901;
+let nodes = 185;
+let supply = 34001;
 let c = 20;
 let wallet_count = 907;
 let burnt = 1284;
 let transactions = 9402;
-
+let avg_tps = 50
 document.getElementById("nodes").innerHTML= nodes;
 document.getElementById("supply").innerHTML= supply;
-document.getElementById("tps").innerHTML= Math.round(((nodes / c)-5)-c);
+document.getElementById("tps").innerHTML= Math.round(((nodes / c)-5)-c) * avg_tps;
 document.getElementById("wallets").innerHTML=  wallet_count;
 /*
 document.getElementById("burnt").innerHTML=  burnt;
-document.getElementById("txns").innerHTML=  transactions;
 */
 document.getElementById('blockList').innerHTML = " <tr> <th>Height</th> <th>Sender</th> <th>Txn Count</th> <th>Total Funds Change</th> <th>Recipitents</th> <th>Timestanp</th> </tr> ";
 function addBlock(height,sender,txn_count,tfc, recipitents,timestamp) {
@@ -27,6 +26,11 @@ function addBlock(height,sender,txn_count,tfc, recipitents,timestamp) {
    document.getElementById('blockList').innerHTML = document.getElementById('blockList').innerHTML + "<tr> <td>"+height+"</td> <td>"+sender+"</td> <td>"+txn_count+"</td> <td>"+ tfc+" AIO</td> <td>"+recipitents+"</td> <td>"+time+"</td> </tr> ";
 }
 
-
+// test
+addBlock(26,"0xe3Cd54f876f6740a39F71afeeesefedD886B1994",12,284, 12,1344345);
+addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
+addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
+addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
+addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
 addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
 addBlock(26,"0xe3Cd54f876f6740a39F71a505cCBD1dD886B1994",12,284, 12,1344345);
