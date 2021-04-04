cf9248221324deb6f7c1eb7cf27267c44b217f46
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2021-04-04 13:25:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,13 @@ client.on('data', function(data) {
 		var rec = JSON.parse(data)
 		if (rec.m_type == "block") {
 			var block = JSON.parse(rec.content)
-			let new_json = `{hash:${block.hash}, time: ${block.timestamp}, public_key: ${block.public_key}, links: [] }`
-			console.log(new_json)
+			if (block.block_type = "Send") {
+				let new_json = `{hash:${block.hash}, time: ${block.header.timestamp}, public_key: ${block.header.public_key}, links: [ {hash: ${block.header.prev_hash}, type: 0 } ] }`
+				console.log(new_json)
+			} else {
+				let new_json = `{hash:${block.hash}, time: ${block.header.timestamp}, public_key: ${block.header.public_key}, links: [ {hash: ${block.header.prev_hash}, type: 0 }, {hash: ${block.send_block}, type: 1 } ] }`
+				console.log(new_json)
+			}
 		}
 	}
 });
