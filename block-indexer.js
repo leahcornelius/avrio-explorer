9c7fd9c7c35189b6f9a9da5414163190379963d8
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2021-04-05 10:46:33 +0100

    Create block-indexer.js

---
+++ block-indexer.js
@@ -0,0 +1,57 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("avrioexplorer");
+  
+var net = require('net');
+
+var client = new net.Socket();
+var init = false;
+client.connect(17785, '127.0.0.1', function () {
+	console.log('Connected to rpc');
+	client.write('init');
+});
+var express = require("express");
+var app = express();
+var cors = require('cors')
+app.use(cors())
+var tot_json = `[`
+app.listen(1234, () => {
+ console.log("API running on port 1234");
+});
+app.get("/dag", (req, res, next) => {
+	res.json(tot_json + "]");
+});
+
+client.on('data', function (data) {
+	console.log('Received: ' + data);
+	if (!init) {
+		console.log("Not init, sending *");
+		client.write('*');
+		init = true;
+	} else {
+		var rec = JSON.parse(data)
+		if (rec.m_type == "block") {
+			var block = JSON.parse(rec.content)
+			if (block.block_type = "Send") {
+				let new_json = `{hash:${block.hash}, time: ${block.header.timestamp}, public_key: ${block.header.chain_key}, links: [ {hash: ${block.header.prev_hash}, type: 0 } ] },`
+				console.log(new_json)
+				tot_json = tot_json + new_json
+			} else {
+				let new_json = `{hash:${block.hash}, time: ${block.header.timestamp}, public_key: ${block.header.chain_key}, links: [ {hash: ${block.header.prev_hash}, type: 0 }, {hash: ${block.send_block}, type: 1 } ] },`
+				console.log(new_json)
+				tot_json = tot_json + new_json
+			}
+		} else {
+			console.log(`Recieved non block msg, type=${rec.m_type}`)
+		}
+		console.log(tot_json)
+	}
+});
+
+client.on('close', function () {
+	console.log('rpc closed');
+});
+});
